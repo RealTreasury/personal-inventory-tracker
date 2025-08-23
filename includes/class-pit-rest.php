6023825be8e785246f4638529fd7c2d41a8229d2
RealTreasury <tschultz@realtreasury.com>
2025-08-23 09:18:16 -0400

    Extend REST API for batch item operations

--- includes/class-pit-rest.php
+++ includes/class-pit-rest.php
@@ -7,10 +7,39 @@ class PIT_REST {
 
     protected function verify_nonce( $request ) {
         $nonce = $request->get_header( 'X-WP-Nonce' );
-        if ( ! $nonce || ! wp_verify_nonce( $nonce, 'wp_rest' ) ) {
+        if ( ! $nonce ) {
+            $nonce = $request->get_param( '_wpnonce' );
+        }
+        if ( $nonce ) {
+            $_REQUEST['_wpnonce'] = $nonce;
+        }
+        if ( false === check_ajax_referer( 'wp_rest', '_wpnonce', false ) ) {
             return new WP_Error( 'pit_invalid_nonce', __( 'Invalid nonce.', 'personal-inventory-tracker' ), array( 'status' => 403 ) );
         }
         return true;
+    }
+
+    public function get_item_schema() {
+        return array(
+            '$schema'    => 'http://json-schema.org/draft-04/schema#',
+            'title'      => 'pit_item',
+            'type'       => 'object',
+            'properties' => array(
+                'id'        => array(
+                    'type'     => 'integer',
+                    'readonly' => true,
+                ),
+                'title'     => array(
+                    'type' => 'string',
+                ),
+                'qty'       => array(
+                    'type' => 'integer',
+                ),
+                'purchased' => array(
+                    'type' => 'boolean',
+                ),
+            ),
+        );
     }
 
     public function register_routes() {
@@ -22,11 +51,27 @@ class PIT_REST {
                     'methods'             => WP_REST_Server::READABLE,
                     'callback'            => array( $this, 'get_items' ),
                     'permission_callback' => array( $this, 'permissions_read' ),
+                    'schema'              => array( $this, 'get_item_schema' ),
                 ),
                 array(
                     'methods'             => WP_REST_Server::CREATABLE,
                     'callback'            => array( $this, 'create_item' ),
                     'permission_callback' => array( $this, 'permissions_write' ),
+                    'args'                => array(
+                        'title'     => array(
+                            'type'     => 'string',
+                            'required' => true,
+                        ),
+                        'qty'       => array(
+                            'type'    => 'integer',
+                            'default' => 0,
+                        ),
+                        'purchased' => array(
+                            'type'    => 'boolean',
+                            'default' => false,
+                        ),
+                    ),
+                    'schema'              => array( $this, 'get_item_schema' ),
                 ),
             )
         );
@@ -39,12 +84,85 @@ class PIT_REST {
                     'methods'             => WP_REST_Server::EDITABLE,
                     'callback'            => array( $this, 'update_item' ),
                     'permission_callback' => array( $this, 'permissions_write' ),
+                    'args'                => array(
+                        'id'        => array(
+                            'type'     => 'integer',
+                            'required' => true,
+                        ),
+                        'title'     => array(
+                            'type' => 'string',
+                        ),
+                        'qty'       => array(
+                            'type' => 'integer',
+                        ),
+                        'purchased' => array(
+                            'type' => 'boolean',
+                        ),
+                    ),
+                    'schema'              => array( $this, 'get_item_schema' ),
                 ),
                 array(
                     'methods'             => WP_REST_Server::DELETABLE,
                     'callback'            => array( $this, 'delete_item' ),
                     'permission_callback' => array( $this, 'permissions_write' ),
+                    'args'                => array(
+                        'id' => array(
+                            'type'     => 'integer',
+                            'required' => true,
+                        ),
+                    ),
                 ),
+            )
+        );
+
+        register_rest_route(
+            'pit/v1',
+            '/items/batch',
+            array(
+                'methods'             => WP_REST_Server::EDITABLE,
+                'callback'            => array( $this, 'batch_update_items' ),
+                'permission_callback' => array( $this, 'permissions_write' ),
+                'args'                => array(
+                    'items' => array(
+                        'type'     => 'array',
+                        'required' => true,
+                        'items'    => array(
+                            'type' => 'object',
+                        ),
+                    ),
+                ),
+                'schema'              => array( $this, 'get_item_schema' ),
+            )
+        );
+
+        register_rest_route(
+            'pit/v1',
+            '/items/import',
+            array(
+                'methods'             => WP_REST_Server::CREATABLE,
+                'callback'            => array( $this, 'import_items' ),
+                'permission_callback' => array( $this, 'permissions_write' ),
+                'args'                => array(
+                    'items' => array(
+                        'type'     => 'array',
+                        'required' => true,
+                        'items'    => array(
+                            'type' => 'object',
+                        ),
+                    ),
+                ),
+                'schema'              => array( $this, 'get_item_schema' ),
+            )
+        );
+
+        register_rest_route(
+            'pit/v1',
+            '/items/export',
+            array(
+                'methods'             => WP_REST_Server::READABLE,
+                'callback'            => array( $this, 'export_items' ),
+                'permission_callback' => array( $this, 'permissions_read' ),
+                'schema'              => array( $this, 'get_item_schema' ),
             )
         );
 
@@ -121,6 +239,67 @@ class PIT_REST {
         update_post_meta( $id, 'purchased', ! empty( $request['purchased'] ) );
 
         return rest_ensure_response( $this->prepare_item( get_post( $id ) ) );
+    }
+
+    public function batch_update_items( $request ) {
+        $updates = $request->get_param( 'items' );
+        $results = array();
+        if ( is_array( $updates ) ) {
+            foreach ( $updates as $update ) {
+                $id = isset( $update['id'] ) ? (int) $update['id'] : 0;
+                if ( ! $id ) {
+                    continue;
+                }
+                if ( isset( $update['title'] ) ) {
+                    wp_update_post(
+                        array(
+                            'ID'         => $id,
+                            'post_title' => sanitize_text_field( $update['title'] ),
+                        )
+                    );
+                }
+                if ( isset( $update['qty'] ) ) {
+                    update_post_meta( $id, 'qty', (int) $update['qty'] );
+                }
+                if ( isset( $update['purchased'] ) ) {
+                    update_post_meta( $id, 'purchased', ! empty( $update['purchased'] ) );
+                }
+                $results[] = $this->prepare_item( get_post( $id ) );
+            }
+        }
+        return rest_ensure_response( $results );
+    }
+
+    public function import_items( $request ) {
+        $items   = $request->get_param( 'items' );
+        $created = array();
+        if ( is_array( $items ) ) {
+            foreach ( $items as $item ) {
+                $id = wp_insert_post(
+                    array(
+                        'post_type'   => 'pit_item',
+                        'post_title'  => sanitize_text_field( $item['title'] ),
+                        'post_status' => 'publish',
+                    ),
+                    true
+                );
+                if ( is_wp_error( $id ) ) {
+                    continue;
+                }
+                if ( isset( $item['qty'] ) ) {
+                    update_post_meta( $id, 'qty', (int) $item['qty'] );
+                }
+                if ( isset( $item['purchased'] ) ) {
+                    update_post_meta( $id, 'purchased', ! empty( $item['purchased'] ) );
+                }
+                $created[] = $this->prepare_item( get_post( $id ) );
+            }
+        }
+        return rest_ensure_response( $created );
+    }
+
+    public function export_items( $request ) {
+        return $this->get_items( $request );
     }
 
     public function update_item( $request ) {
